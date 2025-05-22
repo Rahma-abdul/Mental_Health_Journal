3f6212bd6635c60ff61d3e64f4de0e2a006e7fa1
merna148 <mernanader148@gmail.com>
2025-05-22 15:06:48 +0300

    daily page implementation

--- frontend/src/pages/Sidebar.jsx
+++ frontend/src/pages/Sidebar.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
-import { Clock, CalendarDays, ListTodo, Clipboard  , ChevronRight , ChevronLeft} from 'lucide-react';
-import { NavLink } from 'react-router-dom';
+import { Clock, CalendarDays, ListTodo, Clipboard, ChevronRight, ChevronLeft, LogOut } from 'lucide-react';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 export default function Sidebar( {children}) {
   const [isOpen, setIsOpen] = useState(true); 
+ const navigate = useNavigate();
+  const handleLogout = () => {
+
+    navigate("/login");
+  };
 
   return (
     <div className='flex min-h-screen transition-all duration-300'>
@@ -25,7 +30,7 @@ export default function Sidebar( {children}) {
 
       <aside
       className={`fixed top-0 left-0 z-40 h-screen transition-all duration-300 ease-in-out ${
-        isOpen ? 'w-56'  : 'w-16'
+        isOpen ? 'w-40' : 'w-12'
       } text-white bg-[#ffd4c6]`}
       aria-label="Sidebar"
       >
@@ -41,39 +46,43 @@ export default function Sidebar( {children}) {
         )}
       </button>
 
-        <div className="h-full px-3 py-4 overflow-y-auto">
+        <div className="h-full px-2 py-4 overflow-y-auto">
           <ul className="space-y-5 font-medium">
             <li>
               <NavLink to='/history' className="flex items-center p-2 rounded-lg hover:bg-white/5 group justify-center md:justify-start">
-                <Clock className="w-6 h-6 text-gray-800" /> 
+                <Clock className='w-5 h-5 text-gray-800'/> 
                 <span className={`flex-1 ms-3 whitespace-nowrap text-gray-800 ${!isOpen && 'hidden'}`}>History</span> 
             </NavLink>
             </li>
             <li>
               <NavLink to='/daily' className="flex items-center p-2 rounded-lg hover:bg-white/5 group justify-center md:justify-start">
-                <Clipboard className="w-6 h-6 text-gray-800" /> 
+                <Clipboard className='w-5 h-5 text-gray-800'/> 
                 <span className={`flex-1 ms-3 whitespace-nowrap text-gray-800 ${!isOpen && 'hidden'}`}>Daily Entry</span> 
               </NavLink>
             </li>
             <li>
               <NavLink to='/todo' className="flex items-center p-2 rounded-lg hover:bg-white/5 group justify-center md:justify-start">
-                <ListTodo className="w-6 h-6 text-gray-800" /> 
+                <ListTodo className='w-5 h-5 text-gray-800'/> 
                 <span className={`flex-1 ms-3 whitespace-nowrap text-gray-800 ${!isOpen && 'hidden'}`}>To Do</span> 
               </NavLink>
             </li>
             <li>
               <NavLink to='/calender' className="flex items-center p-2 rounded-lg hover:bg-white/5 group justify-center md:justify-start">
-                <CalendarDays className="w-6 h-6 text-gray-800" /> 
+                <CalendarDays className='w-5 h-5 text-gray-800'/> 
                 <span className={`flex-1 ms-3 whitespace-nowrap text-gray-800 ${!isOpen && 'hidden'}`}>Calendar</span> 
               </NavLink>
             </li>
           </ul>
+           <button onClick={handleLogout} className="flex items-center bg-[#ffd4c6] p-2 mt-8 rounded-lg hover:bg-white/5 justify-center md:justify-start">
+            <LogOut className="w-5 h-5 text-gray-800" />
+            <span className={`ms-3 text-gray-800 ${!isOpen && 'hidden'}`}>Logout</span>
+          </button>
         </div>
       </aside>
 
       {/* Main Content */}
       <main className={`flex-1 transition-all duration-300 ${
-        isOpen ? 'ml-80' : 'ml-16'
+        isOpen ? 'ml-40' : 'ml-12'
       }`}>
         {children}
       </main>
