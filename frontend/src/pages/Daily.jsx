9f6702515fc5b571abde3575239ee553c08b55b7
Rahma-abdul <rahma2002lola@gmail.com>
2025-05-24 03:36:47 +0300

    Issues Fixed

--- frontend/src/pages/Daily.jsx
+++ frontend/src/pages/Daily.jsx
@@ -20,14 +20,14 @@ function Daily() {
     return (
             <Sidebar>
                 <h1>Daily Entry</h1>
-                <div className="container">
-                    <div className="entry">
-                        <div className="header">
-                            <input type='text' className= 'title' placeholder="Today's Title" ></input>
-                            <input type='date' className='date' value={date} onChange={(e) => setDate(e.target.value)} ></input>
+                <div className="daily-container">
+                    <div className="daily-entry">
+                        <div className="daily-header">
+                            <input type='text' className= 'daily-title' placeholder="Today's Title" ></input>
+                            <input type='date' className='daily-date' value={date} onChange={(e) => setDate(e.target.value)} ></input>
                         </div>
 
-                            <textarea className="body" placeholder="How is your day going?" rows={15}></textarea>
+                            <textarea className="daily-body" placeholder="How is your day going?" rows={15}></textarea>
                         
                             {/* <h3>Things to be Grateful for:</h3>
                             <textarea className="body2" placeholder="What are you grateful for today?" rows ={5}></textarea> */}
@@ -35,15 +35,15 @@ function Daily() {
                     </div>
 
                     <div className="side">
-                        <div className="selectmood">
+                        <div className="daily-selectmood">
                             <h3>What's your mood today?</h3>
-                            <div className="moods">
+                            <div className="daily-moods">
                                  {[happy, sad, neutral, angry, emotional].map((icon, index) => (
                                 <img
                                     key={index}
                                     src={icon}
                                     alt="mood"
-                                    className={`icon ${mood === index ? 'selected' : ''}`}
+                                    className={`daily-icon ${mood === index ? 'selected' : ''}`}
                                     onClick={() => setMood(index)}
                                 />
                                  ))}
@@ -51,10 +51,10 @@ function Daily() {
                         </div>
 
 
-                        <div className="music">
+                        <div className="daily-music">
                             <h3>Suggested Song</h3>
-                            <img src={playlist} alt="music" className="playlist"></img>
-                            <div className="details">
+                            <img src={playlist} alt="music" className="daily-playlist"></img>
+                            <div className="daily-details">
                                 {/* Placholder lel backend */}
                                 <p><strong>Title:</strong> Song Name</p>
                                 <p><strong>Artisit:</strong> Artist Name</p>
